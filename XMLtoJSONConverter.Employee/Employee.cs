61d897d574e6148e77d15e93a6c9675a05f3298f
arunabhaMitra <arunabha.official@gmail.com>
2021-08-12 00:29:04 +0530

    Add project files.

---
+++ XMLtoJSONConverter.Employee/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XMLtoJSONConverter.Employee
+{
+    class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
